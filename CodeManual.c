7633ed700da1170b63a74cfe4367200833f70772
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-01-19 11:05:35 +0530

    Add files via upload

---
+++ CodeManual.c
@@ -0,0 +1,23 @@
+#include "ColorPair.h"
+
+void ColorNamesToString(const ColorPair* colorPair, char* MajorColorbuffer, char* MinorrColorbuffer)
+ {
+    sprintf(MajorColorbuffer, "%s", MajorColorNames[colorPair->majorColor]);
+    sprintf(MinorrColorbuffer, "%s", MinorColorNames[colorPair->minorColor]);
+ }
+
+
+void ColourCodeManual()
+ {
+  int TotalColorPairNum = numberOfMajorColors * numberOfMinorColors;
+  printf("COLOR CODE MANUAL\n");
+	for (int ColorNum= 1 ; ColorNum <= TotalColorPairNum ; ColorNumber++)
+	{
+	  ColorPair colorPair = GetColorFromPairNumber(ColorNumber);
+	  char MajorColorName[MAX_COLORPAIR_NAME_CHARS/2];
+	  char MinorColorName[MAX_COLORPAIR_NAME_CHARS/2];  
+	  ColorNamesToString(&colorPair,MajorColorName,MinorColorName);
+	  printf("Pair Number : %d \n Major Color Name : %s\n  Minor Color Name : %s\n", ColorNumber, MajorColorName, MinorColorName);
+	  
+	}
+ }
