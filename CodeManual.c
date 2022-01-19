59f2d94f2f54f978892ed67db3e66b0721f92f89
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-01-19 11:13:29 +0530

    Update CodeManual.c

--- CodeManual.c
+++ CodeManual.c
@@ -12,11 +12,11 @@ void ColourCodeManual()
   printf("COLOR CODE MANUAL\n");
 	for (int ColorNum= 1 ; ColorNum <= TotalColorPairNum ; ColorNum++)
 	{
-	  ColorPair colorPair = GetColorFromPairNumber(ColorNumber);
+	  ColorPair colorPair = GetColorFromPairNumber(ColorNum);
 	  char MajorColorName[MAX_COLORPAIR_NAME_CHARS/2];
 	  char MinorColorName[MAX_COLORPAIR_NAME_CHARS/2];  
 	  ColorNamesToString(&colorPair,MajorColorName,MinorColorName);
-	  printf("Pair Number : %d \n Major Color Name : %s\n  Minor Color Name : %s\n", ColorNumber, MajorColorName, MinorColorName);
+	  printf("Pair Number : %d \n Major Color Name : %s\n  Minor Color Name : %s\n", ColorNum, MajorColorName, MinorColorName);
 	  
 	}
  }
