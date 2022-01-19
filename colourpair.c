6af9a1d39ca857a03c91307edcc15e06d86fd1d0
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-01-19 11:04:50 +0530

    Update colourpair.c

--- colourpair.c
+++ colourpair.c
@@ -10,13 +10,13 @@ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
+    
+    ColourCodeManual();
+    
     return 0;
 }
